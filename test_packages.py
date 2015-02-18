ea3589927b29c075d741322a3e31bfde66bd6fb3
Nikolas Tezak <ntezak@stanford.edu>
2015-02-17 16:20:32 -0800

    added checks for cython and gcc to check_packages.py

--- test_packages.py
+++ test_packages.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import subprocess
+import os
+
 try:
     import IPython
     print("IPython version should be at least 2.4, it is:",
@@ -26,3 +29,25 @@ try:
           pandas.__version__)
 except ImportError:
     print("Please install pandas")
+
+try:
+    import cython
+    print("Cython version should be at least 0.20, it is:",
+          cython.__version__)
+except ImportError:
+    print("Please install pandas")
+
+try:
+    print("Checking for gcc compiler:")
+    print("-"*80)
+    subprocess.call(["gcc", "--version"])
+    print("-"*80)
+    print("success")
+    print("-"*80)
+except OSError as e:
+    if e.errno == os.errno.ENOENT:
+        # handle file not found error.
+        print("Cannot find the gcc compiler")
+    else:
+        # Something else went wrong while trying to run `wget`
+        print("`gcc --version` failed for some reason: ", e)
